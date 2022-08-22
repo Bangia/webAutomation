4600fcf73d8e2c5427d41855c5a1f53cd43b87a4
Bangia <nitinbangia8@gmail.com>
2022-08-22 12:04:24 +0530

    test case added

--- cypress/e2e/Appointment.cy.js
+++ cypress/e2e/Appointment.cy.js
@@ -7,9 +7,9 @@ describe('Test Suite',()=>{
 
     it('Login Test case 1',()=>{
         cy.visit('https://admin.qcaredocs.com/appointments')
-        // cy.get('[name="email"]').type('admin@example.com')
-        // cy.get('[name="password"]').type('12345678')
-        // cy.get('button[type="submit"]').click()
+        cy.get('[name="email"]').type('admin@example.com')
+        cy.get('[name="password"]').type('12345678')
+        cy.get('button[type="submit"]').click()
 
         cy.wait(4000)
         cy.get('[placeholder="Search Appointments"]').type('Akshit')
@@ -20,7 +20,7 @@ describe('Test Suite',()=>{
             cy.log(name)
             cy.log(name.length)
             expect(name).to.contain('Akshit')
-            cy.log("NEW VALUE ADDED");
+            
 
         })
       
